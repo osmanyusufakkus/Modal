8513b0004d2a30f1c9c9c14f4cfd9d5ae5e28808
osmanyusufakkus <osmanyusufakkus@gmail.com>
2022-08-26 13:57:01 +0300

    Add Escape button now closes model

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ const closeModal = function () {
 
 btnClose.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape' && modal.classList.contains('hidden')) {
+  } else {
+    console.log('hiding');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
