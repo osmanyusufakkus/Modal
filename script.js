f57653c497682e02e7635eedb4ac07f2241a14e4
osmanyusufakkus <osmanyusufakkus@gmail.com>
2022-08-26 12:44:03 +0300

    Activate the functions of the on and off buttons
    -Clicking modal button now opens the modal
    -Clicking close button of modal now close the modal
    -Clicking overlay of modal now close the modal

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+
+//Works like array
+const btnsOpen = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('showing');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpen.length; i++) {
+  btnsOpen[i].addEventListener('click', openModal);
+}
+
+const closeModal = function () {
+  console.log('hiding');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
